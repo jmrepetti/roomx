5673cef9de732730dbe572bb9ec73f2fbd94ed86
jmrepetti <jmrepetti@gmail.com>
2020-11-30 20:36:35 +0100

    fetch or create new game on mount

--- lib/roomx_web/live/mines_room_live.ex
+++ lib/roomx_web/live/mines_room_live.ex
@@ -17,22 +17,34 @@ defmodule RoomxWeb.MinesRoomLive do
   end
 
   def discover_cell(socket, value) do
+    socket = socket
+      |> update(:game_state, fn game_state -> Keyword.put(game_state, :discovered, [String.to_integer(value) | game_state[:discovered]] ) end)
+    Roomx.MinesGamesAgent.update(socket.assigns.game_state[:session_id], socket.assigns.game_state)
     socket
-      |> update(:discovered, fn discovered -> [String.to_integer(value) | discovered] end)
   end
 
 
-  @impl true
-  def mount(params, _session, socket) do
-    Phoenix.PubSub.subscribe(Roomx.PubSub, @topic)
-    room = Rooms.get_room_by_uuid!(params["session_id"])
+  def new_game(session_id, socket) do
+    room = Rooms.get_room_by_uuid!(session_id)
     cols = 9
     rows = 9
     total_bombs = 10
     map = Roomx.Mines.generate_map(cols,rows,total_bombs)
     discovered = []
-    state = [ room: room, map: map, cols: cols, rows: rows, total_bombs: total_bombs, discovered: discovered ]
-    {:ok, assign(socket, state)}
+    game_state = [ room: room.name, session_id: session_id, map: map, cols: cols, rows: rows, total_bombs: total_bombs, discovered: discovered ]
+    Roomx.MinesGamesAgent.update(session_id, game_state)
+    {:ok, assign(socket, :game_state, game_state)}
+  end
+
+  @impl true
+  def mount(params, _session, socket) do
+    Phoenix.PubSub.subscribe(Roomx.PubSub, @topic)
+    session_id = params["session_id"]
+    case Roomx.MinesGamesAgent.get(session_id) do
+      nil ->  new_game(session_id, socket)
+      # player join
+      existing_game_state -> {:ok, assign(socket, :game_state, existing_game_state)}
+    end
   end
 
 end
