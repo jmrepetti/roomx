3369ee4406929cfa55f0648e3d4fbc909469a105
jmrepetti <jmrepetti@gmail.com>
2021-09-23 23:45:53 +0200

    Add game_id to pubsub subscrption topic

--- lib/roomx_web/live/mines_room_live.ex
+++ lib/roomx_web/live/mines_room_live.ex
@@ -4,15 +4,17 @@ defmodule RoomxWeb.MinesRoomLive do
   alias Roomx.Rooms
   alias Roomx.Rooms.Room
 
-  @topic "mines_room_event"
+  defp game_topic(socket) do
+    "mines_room_event_#{socket.assigns[:game_state][:game_id]}"
+  end
 
   def handle_event("discover_cell", %{ "cell" => value }, socket) do
-    Phoenix.PubSub.broadcast_from(Roomx.PubSub, self(), @topic, {"discover_cell", %{ "cell" => value } })
-    {:noreply, discover_cell(socket, value)}
+    Phoenix.PubSub.broadcast(Roomx.PubSub, game_topic(socket), {"discover_cell", %{ "cell" => value } })
+    {:noreply, socket}
   end
 
   # handle broadcast message
-  def handle_info({"discover_cell", %{ "cell" => value } } = loquesea, socket) do
+  def handle_info({"discover_cell", %{ "cell" => value }}, socket) do
     {:noreply, discover_cell(socket, value)}
   end
 
@@ -28,23 +30,26 @@ defmodule RoomxWeb.MinesRoomLive do
     room = Rooms.get_room_by_uuid!(game_id)
     cols = 9
     rows = 9
-    total_bombs = 10
+    total_bombs = 6
     map = Roomx.Mines.generate_map(cols,rows,total_bombs)
     discovered = []
+    # discovered = Enum.map(0..cols*rows, &(&1)) #discover all
     game_state = [ room: room.name, game_id: game_id, map: map, cols: cols, rows: rows, total_bombs: total_bombs, discovered: discovered ]
     Roomx.MinesGamesAgent.update(game_id, game_state)
-    {:ok, assign(socket, :game_state, game_state)}
+    assign(socket, :game_state, game_state)
   end
 
   @impl true
   def mount(params, _session, socket) do
-    Phoenix.PubSub.subscribe(Roomx.PubSub, @topic)
     game_id = params["game_id"]
-    case Roomx.MinesGamesAgent.get(game_id) do
-      nil ->  new_game(game_id, socket)
+    socket = case Roomx.MinesGamesAgent.get(game_id) do
+      # _ -> new_game(game_id, socket)
+      nil -> new_game(game_id, socket)
       # player join
-      existing_game_state -> {:ok, assign(socket, :game_state, existing_game_state)}
+      existing_game_state -> assign(socket, :game_state, existing_game_state)
     end
+    Phoenix.PubSub.subscribe(Roomx.PubSub, game_topic(socket))
+    {:ok, socket}
   end
 
 end
