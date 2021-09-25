c684593aefe26882049df59e72c7e1c1ffdc7cd0
jmrepetti <jmrepetti@gmail.com>
2021-09-25 13:56:02 +0200

    Added view helper to provide proper LiveView module name

--- lib/roomx_web/views/room_view.ex
+++ lib/roomx_web/views/room_view.ex
@@ -1,3 +1,11 @@
 defmodule RoomxWeb.RoomView do
   use RoomxWeb, :view
+
+  def activity_live_module(room) do
+    case room.activity do
+      "mines" -> RoomxWeb.RoomMinesLive
+      "tateti" -> RoomxWeb.RoomTatetiLive
+    end
+    # {:safe, content}
+  end
 end
